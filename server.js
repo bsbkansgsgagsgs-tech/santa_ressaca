7a141522baea7df826feb5e56b679f67ea362fb5
bsbkansgsgagsgs-tech <bsbkansgsgagsgs@gmail.com>
2026-02-22 23:54:09 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -139,8 +139,8 @@ async function sendWhatsAppMessage(phone, text) {
 
 
 // Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(__dirname));
 
 // SEO: Rotas limpas que servem o index.html (Google crawl)
